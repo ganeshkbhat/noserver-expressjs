0a72ba0b91f5e98605725597e910f23fbe7ddcc1
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-05-07 20:16:29 +0530

    [COMMIT] Initial commit for express extenders to add getHandler, getHandlers, getHandlerWithMiddlewares. Adding invoker using a depencency supertest. Adding supertest as dependency, will remove later as todo. Adding getBasicNodeMethods for nodejs

--- index.mjs
+++ index.mjs
@@ -1,3 +1,16 @@
+/**
+ * 
+ * Package: noserve, express-noserve
+ * Author: Ganesh B
+ * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
+ * Install: npm i noserve --save , npm i express-noserve --save
+ * Github: https://github.com/ganeshkbhat/noserver-expressjs
+ * npmjs Link: https://www.npmjs.com/package/noserve , https://www.npmjs.com/package/express-noserve
+ * File: index.mjs
+ * File Description: Description: ExpressJS Extend with handler fetchers to invoke without having to listen as a server
+ * 
+ * 
+*/
 
 /* eslint no-console: 0 */
 
--- src/mixins.js
+++ src/mixins.js
@@ -1,4 +1,16 @@
-
+/**
+ * 
+ * Package: noserve, express-noserve
+ * Author: Ganesh B
+ * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
+ * Install: npm i noserve --save , npm i express-noserve --save
+ * Github: https://github.com/ganeshkbhat/noserver-expressjs
+ * npmjs Link: https://www.npmjs.com/package/noserve , https://www.npmjs.com/package/express-noserve
+ * File: mixin.js
+ * File Description: Description: ExpressJS Extend with handler fetchers to invoke without having to listen as a server
+ * 
+ * 
+*/
 
 /* eslint no-console: 0 */
 
